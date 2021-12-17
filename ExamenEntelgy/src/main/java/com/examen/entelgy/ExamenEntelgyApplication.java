747de2db12c8edcc65de35f9594d7bdbe85cba78
Roger2608 <rogercabrera2608@gmail.com>
2021-12-17 01:07:47 -0500

    commit inicial

---
+++ ExamenEntelgy/src/main/java/com/examen/entelgy/ExamenEntelgyApplication.java
@@ -0,0 +1,18 @@
+package com.examen.entelgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ExamenEntelgyApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenEntelgyApplication.class, args);
+	}
+	
+	
+	
+	
+}
